92501ac4c1b7d1e5a0d71a107405d50893a2e8d1
Thái Hoàng Phúc <thaihoangphuc7556@gmail.com>
2024-01-15 21:17:10 +0700

    Update some distributed functions

--- snakegame/engine.cpp
+++ snakegame/engine.cpp
@@ -35,11 +35,105 @@ void StartGame() {
 	STATE = 1;//Start running Thread
 }
 
-void PauseGame(HANDLE t) {
+void ExitGame(HANDLE t) {
 	system("cls");
 	TerminateThread(t, 0);
 }
 
-void ExitGame(HANDLE t) {
+void PauseGame(HANDLE t) {
 	SuspendThread(t);
+}
+
+void ProcessDead() {
+	STATE = 0;
+	GoToXY(0, HEIGH_CONSOLE + 2);
+	printf("Dead, press anykey to continue!");
+}
+
+void DrawSnakeAndFood(char* str) {
+	GoToXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);//Cho con tro nhap nhay
+	printf(".");//Draw food
+	for (int i = 0; i < SIZE_SNAKE; i++)
+	{
+		GoToXY(snake[i].x, snake[i].y);
+		printf(str);
+	}
+}
+
+//Move functions
+
+void MoveRight()
+{
+	if (snake[SIZE_SNAKE - 1].x + 1 == WIDTH_CONSOLE)//if snake hit the wall
+	{
+		ProcessDead();
+	}
+	else {
+		if (snake[SIZE_SNAKE - 1].x + 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) {
+			Eat();
+		}
+		for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+			snake[i].x = snake[i + 1].x;
+			snake[i].y = snake[i + 1].y;
+			
+		}
+		snake[SIZE_SNAKE - 1].x++;
+	}
+}
+
+void MoveLeft()
+{
+	if (snake[SIZE_SNAKE - 1].x -1 == WIDTH_CONSOLE)
+	{
+		ProcessDead();
+	}
+	else {
+		if (snake[SIZE_SNAKE - 1].x -1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) {
+			Eat();
+		}
+		for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+			snake[i].x = snake[i + 1].x;
+			snake[i].y = snake[i + 1].y;
+
+		}
+		snake[SIZE_SNAKE - 1].x--;
+	}
+}
+
+void MoveUp()
+{
+	if (snake[SIZE_SNAKE - 1].y - 1 == HEIGH_CONSOLE)
+	{
+		ProcessDead();
+	}
+	else {
+		if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y -1== food[FOOD_INDEX].y) {
+			Eat();
+		}
+		for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+			snake[i].x = snake[i + 1].x;
+			snake[i].y = snake[i + 1].y;
+
+		}
+		snake[SIZE_SNAKE - 1].y--;
+	}
+}
+
+void MoveDown()
+{
+	if (snake[SIZE_SNAKE - 1].y + 1 == HEIGH_CONSOLE)
+	{
+		ProcessDead();
+	}
+	else {
+		if (snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y + 1 == food[FOOD_INDEX].y) {
+			Eat();
+		}
+		for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+			snake[i].x = snake[i+1].x;
+			snake[i].y = snake[i + 1].y;
+
+		}
+		snake[SIZE_SNAKE - 1].y++;
+	}
 }
